0efd3529b4bc0d30caa3c68c067f2298a6b48a39
sees-usf <50032809+sees-usf@users.noreply.github.com>
2020-10-09 14:46:31 -0400

    Update logging.py

--- SeqMining-Python/src/logging.py
+++ SeqMining-Python/src/logging.py
@@ -1,7 +1,8 @@
 import sys
 
-DEBUG = 'debug'
+DEBUG = 'debug1'
 INFO = 'info'
+WARN = 'warning'
 
 def log2file(filename, input, type='info'):
     fp = open(filename, 'w')
@@ -15,5 +16,7 @@ def log2file(filename, input, type='info'):
 def log(input, type='info'):
     if type.lower() == 'debug':
         print('** DEBUG **', input, end='')
+    elif type.lower() == 'warning':
+        print('* WARNING *', input, end='')
     elif type.lower() == 'info':
         print(input, end='')
