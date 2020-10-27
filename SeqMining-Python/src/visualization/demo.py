14caa7800fc3378868c52465039fe4c09dacf726
sees-usf <50032809+sees-usf@users.noreply.github.com>
2020-10-27 10:14:12 -0400

    Update demo.py

--- SeqMining-Python/src/visualization/demo.py
+++ SeqMining-Python/src/visualization/demo.py
@@ -1,8 +1,24 @@
 from src.planterUML import Planter
 
+prefix_string = input('Prefix for sequences to show: ')
+prefix = []
+prefix_string = prefix_string.split()
+for e in prefix_string:
+    prefix.append(int(e))
+print(prefix)
+
+
+detail_string = 0
+detail_string = input('Details in the graphs (0: no message definition, 1: with message definition: ')
+detail_level = detail_string
+
 pt = Planter()
-pt.prefix = [0, 18, 10, 11] # specify the prefix for which UML will be drawn (has default [0, 18, 10])
+# pt.prefix = [0, 18, 10, 11] # specify the prefix for which UML will be drawn (has default [0, 18, 10])
+pt.prefix = prefix
+
 #following two lines can be commented out, and then deafult large.msg and sol-sequences.txt will be used (these files can be found in the src folder)
 pt.msg_file = './tests/def.msg' # specify the msg definition file (default is large.msg), best practice is to specify the starting and terminating messages in this file by #
 pt.sol_file = './tests/sol.txt'# specify the solution file
-pt.draw(detailed=0) #call the api, detailed = 1 means nodes complete definition, if detailed = 0(which is default), only number in the sequences will be there. The purpose is to prevent UMLs from being cut off.
+
+# pt.draw(detailed=0) #call the api, detailed = 1 means nodes complete definition, if detailed = 0(which is default), only number in the sequences will be there. The purpose is to prevent UMLs from being cut off.
+pt.draw(detail_level)
