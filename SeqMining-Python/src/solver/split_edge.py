ac332758d2f692f8f7b765e7cdba67b9f56099ba
sees-usf <50032809+sees-usf@users.noreply.github.com>
2020-10-27 10:10:01 -0400

    Create split_edge.py
    new file, used to represent edges in CG in different flows.

---
+++ SeqMining-Python/src/solver/split_edge.py
@@ -0,0 +1,26 @@
+import copy
+from src.logging import *
+from z3 import *
+
+#@ A type of edges which have instances of the same edge in the original causality graph
+#@ one for each sequnece
+#@
+class split_edge:
+    def __init__(self, g_edge, s_index):
+        self.graph_edge = g_edge
+        self.seq_index = s_index
+        self.z3var = Int(self.graph_edge.get_id() + '_' + str(self.seq_index))
+
+    
+    def get_index(self):
+        return self.seq_index
+
+    def get_z3var(self):
+        return self.z3var
+
+        
+
+        
+        
+
+    
