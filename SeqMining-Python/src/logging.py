810c2f7539aa2d83470566c900bb6eb28916cff0
sees-usf <50032809+sees-usf@users.noreply.github.com>
2020-11-06 22:21:46 -0500

    Update logging.py
    made logging code to show calling function name and line number

--- SeqMining-Python/src/logging.py
+++ SeqMining-Python/src/logging.py
@@ -1,22 +1,56 @@
 import sys
+import inspect
 
-DEBUG = 'debug'
+
+DEBUG = 'debug1'
 INFO = 'info'
 WARN = 'warning'
 
+#@ return the object.function name where this function is called.
+def whoami():
+    stack = inspect.stack()
+    the_class = stack[1][0].f_locals["self"].__class__.__name__
+    the_method = stack[1][0].f_code.co_name
+
+    return the_class + ':' + the_method
+
+    # callerObj = inspect.currentframe().f_back.f_locals['self']
+    # callerFunc = inspect.stack()[1][3]
+    # return str(callerObj) +':'+callerFunc
+
+
+#@ return the line number where this function is called.
+def line_numb():
+   '''Returns the current line number in our program'''
+   return inspect.currentframe().f_back.f_lineno
+
+
 def log2file(filename, input, type='info'):
-    fp = open(filename, 'w')
+    try:
+        fp = open(filename, 'w')
+    except IOError as e:
+        print("Couldn't open file (%s)." % e)
+        return
+
     if type.lower() == 'debug':
-        fp.write('** DEBUG **', input)
+        fp.write('* DEBUG *', input)
+    elif type.lower() == 'warning':
+        print('** WARNING **', input, end='')
     elif type.lower() == 'info':
         fp.write(input)
     fp.close() 
 
 
-def log(input, type='info'):
+def log(inp, type='info', stop=False):
     if type.lower() == 'debug':
-        print('** DEBUG **', input, end='')
+        print('* DEBUG *', inp, end='')
+        if stop: input('Stop, hit a key to continue ->')
     elif type.lower() == 'warning':
-        print('* WARNING *', input, end='')
+        print('** WARNING **', inp, end='')
+        if stop: input('Stop, hit a key to continue ->')
     elif type.lower() == 'info':
-        print(input, end='')
+        print(inp, end='')
+        if stop: input('Stop, hit a key to continue ->')
+
+
+
