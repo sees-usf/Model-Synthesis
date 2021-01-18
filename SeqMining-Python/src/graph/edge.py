ac888d544368b0862dba66ddd802575102297fee
sees-usf <50032809+sees-usf@users.noreply.github.com>
2021-01-18 14:51:29 -0500

    Update edge.py

--- SeqMining-Python/src/graph/edge.py
+++ SeqMining-Python/src/graph/edge.py
@@ -5,11 +5,21 @@ class Edge:
     def __init__(self, graph, src, dest):
         self.graph = graph
         self.id = src.get_symbol_index() + "_" + dest.get_symbol_index()
+
+        ## nodes in the causality_graph representing messages with potential causality 
         self.source = src
         self.destination = dest
-        self.support = 0
+
+        ## support is a list of index pairs of positions of src/dest in the trace
+        ## its length gives support count
+        self.support = None
+
+        ## An approximate number of edges where src/dest happen w/o other causal messages 
+        self.direct_support_count = 0
+
         self.forward_conf = 0
         self.backward_conf = 0
+        self.mean_conf = 0
         self.ranking = 100
         self.z3var = Int(self.id)
         self.source.add_outgoing_edge(self)
@@ -27,11 +37,23 @@ class Edge:
     def get_destination(self):
         return self.destination
 
-    def get_edge_support(self):
+    def get_support(self):
+        return 0 if self.support is None else len(self.support)
+
+    def get_support_pos(self):
         return self.support
 
-    def get_support(self):
-        return self.support
+    def get_direct_support(self):
+        return self.direct_support_count
+
+    def get_fconf(self):
+        return self.forward_conf
+
+    def get_bconf(self):
+        return self.backward_conf
+
+    def get_hconf(self):
+        return self.mean_conf
 
     def get_z3var(self):
         return self.z3var
@@ -39,16 +61,21 @@ class Edge:
     def get_ranking(self):
         return self.ranking
 
-    def set_edge_support(self, value):
-        self.support = value
+    def set_direct_support(self, value):
+        self.direct_support_count = value
 
     def set_ranking(self, value):
         self.ranking = value
 
     def set_support(self, value):
         self.support = value
-        self.forward_conf = (self.support/self.source.get_support())
-        self.backward_conf = (self.support/self.destination.get_support())
+        self.forward_conf = (self.get_support()/self.source.get_support())
+        self.backward_conf = (self.get_support()/self.destination.get_support())
+        if self.get_support() != 0:
+            self.mean_conf = (2 * self.forward_conf * self.backward_conf)/(self.forward_conf + self.backward_conf)
 
     def equals(self, edge):
         return self.id == edge.id
+
+    def print_full(self):
+        return "{:<45}".format(self.get_source().print_full()) + ' -->  ' + "{:<45}".format(self.get_destination().print_full())
